ca6b3de1b06cac39c3422ce0fc61fb1be20cdb37
John Knotts <jhnnyk@gmail.com>
2024-09-08 08:43:22 -0600

    gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('hello world!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 55, 0, 150),
+              Color.fromARGB(255, 248, 56, 254),
+            ]),
+          ),
+          child: const Center(
+            child: Text('hello world!'),
+          ),
         ),
       ),
     ),
