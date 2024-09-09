ea9762faf6c19303c7f64c477845cc3fa21a94cb
John Knotts <jhnnyk@gmail.com>
2024-09-09 05:31:21 -0600

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,23 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 55, 0, 150),
-              Color.fromARGB(255, 248, 56, 254),
-            ]),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 55, 0, 150),
+                Color.fromARGB(255, 248, 56, 254),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
           child: const Center(
-            child: Text('hello world!'),
+            child: Text(
+              'hello world!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
