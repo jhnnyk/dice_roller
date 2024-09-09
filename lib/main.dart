30aee314039024ccf8e9757fa4cd674b2cee9577
John Knotts <jhnnyk@gmail.com>
2024-09-09 15:10:48 -0600

    splitting code across files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dice_roller/gradient_container.dart';
 
 void main() {
   runApp(
@@ -10,31 +11,4 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
 
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 55, 0, 150),
-            Color.fromARGB(255, 248, 56, 254),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'hello world!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
-}
